3eda1ff40e941138554a7547cc67bb565b38167f
omfghello <122947009+omfghello@users.noreply.github.com>
2025-03-11 14:20:18 -0400

    Update INDEX.py

--- INDEX.py
+++ INDEX.py
@@ -1,24 +1,23 @@
 import discord
 from discord.ext import commands
 
+description = "a bot"
+status = discord.Status.online
+activity = "testing bots"
+
 import discord.state
 
 class MyClient(discord.Client):
     async def on_ready(self):
+        await client.change_presence(activity=discord.Game(name=activity))
         print("bot logged in as client")
-
-if ConnectionError:
-     
-     print("Bot failed to connect to client")
-
-
-description = "a bot"
-status = discord.Status.online
-activity = "testing bots"
+        
+        if ConnectionError == True:
+            print("Bot failed to connect to client")
 
 activity = discord.Game(name= description)
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
-client.run('token')
+client.run('Token')
