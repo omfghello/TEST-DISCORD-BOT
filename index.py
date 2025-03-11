a4c6fe372018be10a88af61c1ccc8a9b731cbd9b
omfghello <122947009+omfghello@users.noreply.github.com>
2025-03-10 20:43:49 -0400

    Add files via upload

---
+++ index.py
@@ -0,0 +1,13 @@
+import random
+
+responses = ["Hell", "Greetings!", "Hi, how can I help you?", "Hey!"]
+
+user_input = input("Enter your message: ")
+
+if user_input == "Hell":
+ 
+ print("You're in hell!")
+
+else:
+ 
+ print("You're not in hell!")
